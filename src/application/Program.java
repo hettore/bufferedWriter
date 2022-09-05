2d60fcfd088e017cac3f33b0f3457c79fc172c92
Hettore Eduardo <duh.barretoo@gmail.com>
2022-09-05 15:15:59 -0300

    Exemplo do uso do bufferedWriter e fileWriter.

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+		
+		String path = "/Volumes/Ssd/Users/hettoreeduardo/out2";
+		// esse true faz com que o texto seja adicionado cada vez que ao gerar um arquivo novo.(mesmo arquivo)
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
